76dca68d93f336a68af1001b0ed059849fd6e3e8
Angelo Annunziata <ing.annunziata@gmail.com>
2019-11-02 23:52:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 var $videoWrapper;
 var $videoPoster;
 var $closeBtn;
+var $hamburgerMenu;
+var $navBar;
 
 console.log("TODO: JS not loaded for missing resources (CodeSandbox Issue)...");
 
 document.addEventListener("DOMContentLoaded", DOMReadyHandler);
 
 function DOMReadyHandler() {
+	initModal();
+	initHamburgerMenu();
+}
+
+function initHamburgerMenu() {
+	$hamburgerMenu = document.querySelector(".hamburger-menu");
+	$navBar = document.getElementById("nav-bar");
+	$hamburgerMenu.addEventListener("click", showHideMobileMenu);
+}
+
+function showHideMobileMenu() {
+	$navBar.classList.toggle("visible");
+}
+
+function initModal() {
 	$videoWrapper = document.querySelector(".video-wrapper");
 	$videoPoster = document.querySelector(".video-poster");
 	$closeBtn = document.querySelector(".close-btn");
@@ -16,11 +33,9 @@ function DOMReadyHandler() {
 }
 
 function showVideoModal() {
-	debugger;
 	$videoWrapper.classList.add("visible");
 }
 
 function closeVideoModal() {
-	debugger;
 	$videoWrapper.classList.remove("visible");
 }
